67be26180c78e34c13576d777ae385d35f4b24ad
MotNam <motokonamba@gmail.com>
2022-03-03 01:39:29 +0100

    second commit ex1 to ex6 finished

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,17 @@
-// Iteration 1: All directors? - Get the array of all directors.
-// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+// --->Iteration 1: All directors? - Get the array of all directors.
 
 
 const movies = require("./data");
-const newArray = movies.map (movies => movies.director);
+
+const newArray = movies.map (item => item.director);
 console.log (newArray);
-   
+  
 
-// How could you "clean" a bit this array and make it unified (without duplicates)?
 
-//const movies = require("./data");
-//const newArray = movies.map (movies => movies.director);
+
+
+// --->// _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
+//How could you "clean" a bit this array and make it unified (without duplicates)?
 
 function cleanArray(array) {
 let noDupsArray = [];
@@ -24,46 +25,98 @@ return noDupsArray;
 }
 console.log (cleanArray (newArray))
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-
-const updatedArr = (cleanArray (newArray));
-
-function howManyMovies(array) {
- const ssSelection = updatedArr.filter (updatedArr => updatedArr.director === 'Steven Spielberg' 
-  && updatedArr.genre === 'Drama');
-return ssSelection;
-}
-console.log (howManyMovies (updatedArr))
 
 
 
-// Iteration 3: All scores average - Get the average of all scores with 2 decimals
-/*const totalReviews = product.reviews.reduce(function (acc, review) {
-  return acc + review.rate;
-}, 0); // starts acc to 0
 
-console.log(`averageRate rate: ${averageRate}`); // <== averageRate rate:  3.2
-*/
+// ---> Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
 
-function scoresAverage(array) {
-  const totalScore = movies.score.reduce (function (acc, score){
-  return acc + score.rate;
-  }, 0);
-  const Average = totalScore / movies.score.length;  
+const howManyMovies = movies.filter (movies => movies.director.includes('Steven Spielberg')
+&& movies.genre.includes ('Drama'))
+
+console.log (howManyMovies);
+
+
+
+
+
+
+//---> Iteration 3: All scores average - Get the average of all scores with 2 decimals
+
+const totalScore = movies.reduce ((acc, currentValue) => {
+  return acc += currentValue.score; 
+}, 0);
+const allAvg = totalScore / movies.length;  
+console.log (allAvg.toFixed(2));
+
+
+
+
+//---> Iteration 4: Drama movies - Get the average of Drama Movies
+
+const DramaMovies = movies.filter (item => item.genre.includes("Drama"));
+//console.log(DramaMovies);
+
+const DramaAvg = DramaMovies.reduce((acc, currentValue) => {
+  return acc += currentValue.score; 
+}, 0)/ DramaMovies.length;  
+
+console.log (DramaAvg.toFixed(2));
+
+
+
+
+
+//----> Iteration 5: Ordering by year - Order by year, ascending (in growing order)
+//Create a function orderByYear() that receives an array as parameter and returns a new sorted array.
+//If two movies have the same year, order them in alphabetical order by their title! 
+const listByYear = movies.map (movie => {
+  const listByYear = movie.year;
+  const listByTitle = movie.title;
+  return{
+    year: listByYear, 
+    title: listByTitle,
   }
-console.log (scoresAverage (movies.score))
+});
+const sortedList = listByYear.sort((a , b) => (a.year - b.year));
+//console.log(sortedList);
+const finalL = sortedList.sort();
+console.log(finalL);
 
-// Iteration 4: Drama movies - Get the average of Drama Movies
-// As in #3 ?
-function dramaMoviesScore() {}
 
-// Iteration 5: Ordering by year - Order by year, ascending (in growing order)
-//*---SORT
-function orderByYear() {}
 
-// Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
-//*---SORT
-function orderAlphabetically() {}
+
+// --->Iteration 6: Alphabetic Order - Order by title and print the first 20 titles
+
+//List with titles only 
+const listOfTitles = movies.map(movie => {
+  const myList = movie.title;
+  return {
+    title: myList
+  }
+})
+//console.log(listOfTitles);
+
+//Get titles into Array
+//console.log(Array.from(listOfTitles));  
+
+//Order alphabetically and Print out 20 titles
+const myNewList = Array.from(listOfTitles);
+myNewList.sort();
+//console.log(myNewList);
+
+if (myNewList.length > 20 ){
+  myNewList.length = 20 ;
+}
+console.log(myNewList);
+
+
+
+/*____END____*/
+
+
+
+
 
 // BONUS - Iteration 7: Time Format - Turn duration of the movies from hours to minutes
 function turnHoursToMinutes() {}
